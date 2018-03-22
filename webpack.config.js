0545cea2a2297b7f2c52a1566666555ed708b85e
夏宇霆 <56833517@qq.com>
2018-03-22 16:32:00 +0800

    添加了md文件，添加了HMR功能

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,30 @@ module.exports = {
   entry: {
     app: path.join(__dirname, "src", "index.js")
   },
+  mode: "development", //development' or 'production'
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "dist")
   },
   devServer: {
+    proxy: {
+      "/api": {
+        target: "https://xiayuting.cc",
+        secure: false
+      }
+    },
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    inline: true,
+    hot: true,
+    stats: "minimal",
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 8080,
     host: "0.0.0.0",
     historyApiFallback: true
-  }
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
